02705870414d160e0446ed166c1b2edb93db0177
David Tahim <davidtahim@gmail.com>
2023-04-27 08:59:17 -0300

    informações do frete

--- frete.php
+++ frete.php
@@ -4,9 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Oficina Mecânica</title>
+    <link rel="stylesheet" href="../css/principal.css">
 </head>
 <body>
-    
+    <table>
+        <tr>
+        <td>Distância</td>
+        <td>Custo</td>
+        </tr>
+        <tr>
+        <td>50</td>
+        <td>5</td>
+        </tr>
+        <tr>
+        <td>100</td>
+        <td>10</td>
+        </tr>
+        <tr>
+        <td>150</td>
+        <td>15</td>
+        </tr>
+        <tr>
+        <td>200</td>
+        <td>20</td>
+        </tr>
+        <tr>
+        <td>250</td>
+        <td>25</td>
+        </tr>
+    </table>    
 </body>
 </html>
